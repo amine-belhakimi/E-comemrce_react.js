345be973f2bd3ea3a174c72569febfc44de7b729
Amine <aminebelhakimi@hotmail.com>
2021-02-10 22:44:56 +0100

    adding this old project to github

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -22,7 +22,7 @@ const Header = ({ currentUser, hidden }) => (
   <div>
       <nav class="navbar navbar-expand-md navbar-light bg-light">
       <a class="navbar-brand" href="#"><Link  to='/'>
-                  <Logo  />
+                  <Logo style={{width:60,height:60}} />
                   </Link></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -13,10 +13,9 @@ const StripeCheckoutButton = ({ price }) => {
   return (
     <StripeCheckout
       label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      name='King Ltd.'
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel='Pay Now'
